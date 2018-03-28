0f2b496d3f9de7f63e600a72e4b5f5f4a04bda13
Tito Yslas <roberto.yslas@gmail.com>
2018-03-28 11:30:19 -0600

    base Burger class addded

---
+++ oop-master-challenge/src/com/tito/Burger.java
@@ -0,0 +1,88 @@
+package com.tito;
+
+public class Burger {
+  private String bun;
+  private String meat;
+  private boolean lettuce;
+  private boolean tomato;
+  private boolean avocado;
+  private boolean onion;
+  private int basePrice;
+  private int lettucePrice;
+  private int tomatoPrice;
+  private int avoPrice;
+  private int onionPrice;
+  private int addtnsPrice;
+  private int totalPrice;
+
+
+  public Burger(boolean lettuce, boolean tomato, boolean avocado, boolean onion) {
+    this.bun = "brioche";
+    this.meat = "beef";
+    this.lettuce = lettuce;
+    this.tomato = tomato;
+    this.avocado = avocado;
+    this.onion = onion;
+    this.basePrice = 10;
+    this.lettucePrice = 1;
+    this.tomatoPrice = 1;
+    this.avoPrice = 2;
+    this.onionPrice = 1;
+
+    System.out.println("base price: $" + this.basePrice + "\n");
+    addLettuce(lettuce);
+    addTomato(tomato);
+    addAvocado(avocado);
+    addOnion(onion);
+
+    this.totalPrice = this.basePrice + this.addtnsPrice;
+  }
+
+  private void addLettuce(boolean lettuce) {
+    if (lettuce) this.addtnsPrice += this.lettucePrice;
+    System.out.println("adding lettuce for price of $" + this.lettucePrice);
+  }
+
+  private void addTomato(boolean tomato) {
+    if (tomato) this.addtnsPrice += this.tomatoPrice;
+    System.out.println("adding tomato for price of $" + this.tomatoPrice);
+  }
+
+  private void addAvocado(boolean avocado) {
+    if (lettuce) this.addtnsPrice += this.avoPrice;
+    System.out.println("adding avocado for price of $" + this.avoPrice);
+  }
+
+  private void addOnion(boolean onion) {
+    if (onion) this.addtnsPrice += this.onionPrice;
+    System.out.println("adding onion for price of $" + this.onionPrice);
+  }
+
+  public int getBasePrice() {
+    return basePrice;
+  }
+
+  public int getLettucePrice() {
+    return lettucePrice;
+  }
+
+  public int getTomatoPrice() {
+    return tomatoPrice;
+  }
+
+  public int getAvoPrice() {
+    return avoPrice;
+  }
+
+  public int getOnionPrice() {
+    return onionPrice;
+  }
+
+  public int getAddtnsPrice() {
+    return addtnsPrice;
+  }
+
+  public int getTotalPrice() {
+    return totalPrice;
+  }
+}
---
+++ oop-master-challenge/src/com/tito/Main.java
@@ -0,0 +1,29 @@
+package com.tito;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // The purpose of the application: help Bill's Burgers manage their hamburger selling process
+    // Our application will help Bill select types of burgers, add'tnl ingredients, and calculate price
+    // We will have a base burger along with two of the other most popular burgers at Bill's
+    // The base burger will have the following items:
+      // Bun type, meat, and up to 4 add'tns (e.g. lettuce, tomato, carrot, etc.) to be selected by each guest
+    // Each add'tnl item increases the price, which will be tracked and included in the calculation of the total price
+    // The burger has a base price and all add'tns have their individual price
+    // A Hamburger class will handle all the above
+    // The constructor should only include the bun type, meat, and price - can also include burger name or use a setter
+    // Also create two extra types of burgers (subclasses) that cater to:
+      // a) Healthy burger (on a brown rye bread roll), plus two add'tnl items (up to 6 add'tnl ingredients)
+      // hint: you probably want to process the two add'tnl ingredients in this new subclass, not the base class
+      // b) Deluxe hamburger - comes with chips & drink as add'tns, w/no other add'tns allowed
+      // hint: auto-magically add chips & drink at instantiation of the deluxe burger object & prevent further add'tns
+    //  All 3 classes will have a method to show the base price plus any add'tns:
+      // show the add'tn name, add'tn price, and total price (base price + all add'tns)
+    // For the two add'tnl classes you may need to get pricing for the base class, then add add'tns to find the total
+
+    Burger burger = new Burger(true, true, true, true);
+    System.out.println("base price: $" + burger.getBasePrice());
+    System.out.println("additions price: $" + burger.getAddtnsPrice());
+    System.out.println("total price: $" + burger.getTotalPrice());
+  }
+}
